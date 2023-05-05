7fa56f234e17b93608c72faa74d7bc3135b339a5
htorricotecsup <53871912+htorrico@users.noreply.github.com>
2023-05-05 14:27:35 -0500

    Add project files.

---
+++ Lab062023B/Lab062023B/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab062023B
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            List<string> colores = new List<string>();
+
+            colores.Add("Red");
+            colores.Add("Yellow");
+            colores.Add("Blue");
+            colores.Add("Green");
+            colores.Add("White");
+
+
+            lvColores.ItemsSource = colores;
+
+        }
+    }
+}
